7b03493663224537dae5d05163e84be314d99935
Flávio Coutinho <fegemo@gmailcom>
2019-10-17 16:35:49 -0300

    Finishes creating post-process exercise

--- core/assets/shaders/post-process.fragment.glsl
+++ core/assets/shaders/post-process.fragment.glsl
@@ -5,31 +5,6 @@ uniform sampler2D u_texture;
 varying vec2 v_texCoords;
 varying vec3 v_fragPosition;
 
-// retorna cor invertida
-vec3 inverter(vec3 cor) {
-    // não é preciso verificar se está negativo porque o GLSL faz "clamp" 
-    // do valor no intervalo [0, 1]
-    return vec3(1.0 - cor);
-}
-
-// retorna cor em escala de cinza
-vec3 paraCinza(vec3 cor) {
-    // float tomDeCinza = dot(vec3(0.33), color);                // versão "naive"
-    float tomDeCinza = dot(vec3(0.2989, 0.5879, 0.1140), cor); // versão mais correta
-    return vec3(tomDeCinza);
-}
-
-// retorna cor em modo sépia
-vec3 paraSepia(vec3 cor) {
-    mat3 transformacaoSepia = mat3(
-        0.393, 0.769, 0.189,
-        0.349, 0.686, 0.168,
-        0.272, 0.534, 0.131
-    );
-
-    return transpose(transformacaoSepia) * cor;
-    //return vec3[]
-}
 
 // aplica uma convolução no pixel usando o kernel especificado
 vec3 convolucao(sampler2D tex, vec2 texCoords, float[9] kernel) {
@@ -68,86 +43,14 @@ vec3 convolucao(sampler2D tex, vec2 texCoords, float[9] kernel) {
     return corResultante;
 }
 
-vec3 borrar(sampler2D tex, vec2 texCoords) {
 
-    
-    // kernel de blur
-    // |  1, 2, 1  |
-    // |  2, 4, 2  | /16.0
-    // |  1, 2, 1  |
-    float peso = 1.0 / 16.0;
-    float kernel[9];
-    kernel[0] = peso*1.0;
-    kernel[1] = peso*2.0;
-    kernel[2] = peso*1.0;
-
-    kernel[3] = peso*2.0;
-    kernel[4] = peso*4.0;
-    kernel[5] = peso*2.0;
-
-    kernel[6] = peso*1.0;
-    kernel[7] = peso*2.0;
-    kernel[8] = peso*1.0;
-
-    return convolucao(tex, texCoords, kernel);
-}
-
-vec3 agucar(sampler2D tex, vec2 texCoords) {
-    // kernel de aguçar imagem (sharpen)
-    //
-    //    -1, -1, -1,
-    //    -1,  9, -1,
-    //    -1, -1, -1
-    //
-    float kernel[9];
-    kernel[0] = -1.0;
-    kernel[1] = -1.0;
-    kernel[2] = -1.0;
-
-    kernel[3] = -1.0;
-    kernel[4] =  9.0;
-    kernel[5] = -1.0;
-
-    kernel[6] = -1.0;
-    kernel[7] = -1.0;
-    kernel[8] = -1.0;
-
-    return convolucao(tex, texCoords, kernel);
-}
-
-vec3 bordas(sampler2D tex, vec2 texCoords) {
-    // kernel de detectar bordas
-    //
-    //     1,  1,  1,
-    //     1, -9,  1,
-    //     1,  1,  1
-    //
-    float kernel[9];
-    kernel[0] =  1.0;
-    kernel[1] =  1.0;
-    kernel[2] =  1.0;
-
-    kernel[3] =  1.0;
-    kernel[4] = -9.0;
-    kernel[5] =  1.0;
-
-    kernel[6] =  1.0;
-    kernel[7] =  1.0;
-    kernel[8] =  1.0;
-
-    return convolucao(tex, texCoords, kernel);
-}
-
-vec3 toonShading(sampler2D tex, vec2 texCoords) {
-    vec3 contorno = inverter(paraCinza(bordas(u_texture, v_texCoords)));
-    vec3 cor;
-    if (length(contorno) < 0.7) {
-        cor = vec3(0.0);
-    } else {
-        cor = texture2D(tex, texCoords).rgb;
-    }
+// retorna cor invertida
+vec3 inverter(vec3 cor) {
+    // não é preciso verificar se está negativo porque o GLSL faz "clamp" 
+    // do valor no intervalo [0, 1]
     return cor;
 }
+
 
 void main() {
     vec3 corDoPixel = texture2D(u_texture, v_texCoords).rgb;

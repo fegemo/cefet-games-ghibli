02a5c1caa476c07dba2761ae4196feeea35e646a
Flávio Coutinho <fegemo@gmailcom>
2019-10-17 10:44:24 -0300

    Adds exercise - initial version

--- core/assets/shaders/post-process.fragment.glsl
+++ core/assets/shaders/post-process.fragment.glsl
@@ -5,16 +5,19 @@ uniform float u_time;
 varying vec2 v_texCoords;
 varying vec3 v_fragPosition;
 
+// retorna cor invertida
 vec3 invert(vec3 color) {
-    return vec3(1.0 - color);
+    return color;
 }
 
+// retorna cor em escala de cinza
 vec3 toGrayscale(vec3 color) {
-    float grayShade = dot(vec3(0.2989, 0.5870, 0.1140), color);
-    return vec3(grayShade);
+    return color;
 }
 
 vec3 blur(sampler2D tex, vec2 texCoords) {
+    // cria um vetor 3x3 contendo o deslocamento de cada pixel adjacente a este
+    // (do kernel)
     float offset = 1.0 / 300.0;
     vec2 kernelOffsets[9] = vec2[](
         vec2(-offset,  offset),      // cima-esquerda
@@ -28,30 +31,38 @@ vec3 blur(sampler2D tex, vec2 texCoords) {
         vec2( offset, -offset)       // baixo-direita
     );
 
-    float constantWeight = 1.0 / 16.0;
     
-    float kernelWeights3[9] = float[](
+    // kernel de blur
+    float constantWeight = 1.0 / 16.0;
+    float kernelWeights[9] = float[](
         constantWeight,   constantWeight*2, constantWeight,
         constantWeight*2, constantWeight,   constantWeight*2,
         constantWeight,   constantWeight*2, constantWeight
     );
-    float kernelWeights2[9] = float[](
-        -1, -1, -1,
-        -1,  9, -1,
-        -1, -1, -1
-    );
 
-    float kernelWeights[9] = float[](
-         1,  1,  1,
-         1, -9,  1,
-         1,  1,  1
-    );
+    // kernel de aguçar imagem (sharpen)
+    //float kernelWeights[9] = float[](
+    //    -1, -1, -1,
+    //    -1,  9, -1,
+    //    -1, -1, -1
+    //);
 
+    // kernel de detectar bordas
+    //float kernelWeight[9] = float[](
+    //     1,  1,  1,
+    //     1, -9,  1,
+    //     1,  1,  1
+    //);
+
+    // olha na textura quais são as cores dos vizinhos deste pixel
     vec3 neighborsColors[9];
     for (int i = 0; i < 9; i++) {
         neighborsColors[i] = texture(tex, texCoords + kernelOffsets[i]);
     }
 
+    // aplica a convolução, fazendo com que a cor resultante deste pixel
+    // seja uma combinação das cores dos pixels adjacentes (3x3) multiplicadas
+    // pelos pesos (do kernel)
     vec3 resultingColor = vec3(0.0);
     for (int i = 0; i < 9; i++) {
         resultingColor += neighborsColors[i] * kernelWeights[i];
@@ -62,16 +73,5 @@ vec3 blur(sampler2D tex, vec2 texCoords) {
 
 void main() {
     vec3 colorFromTexture = texture(u_texture, v_texCoords);
-    //gl_FragColor = vec4(invert(colorFromTexture), 1.0);
-    //gl_FragColor = vec4(toGrayscale(colorFromTexture), 1.0);
-    vec3 outlineColor = invert(toGrayscale(blur(u_texture, v_texCoords)));
-    gl_FragColor = vec4(outlineColor, 1.0);
-    if (length(outlineColor) < 1.5) {
-        // é um contorno
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-    } else {
-        //gl_FragColor = vec4(1.0);
-        gl_FragColor = vec4(colorFromTexture, 1.0);
-    }
-    //gl_FragColor = vec4(mix(outlineColor, colorFromTexture, length(outlineColor) < 1.5 ? 0.0 : 1.0), 1.0);
+    gl_FragColor = vec4(invert(colorFromTexture), 1.0);
 }
